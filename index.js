914a646bc6599a25a4803ea9205cdb82f9daa667
mrunalibind <mrunalibind123@gmail.com>
2023-05-16 23:25:09 +0530

    3

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-let ham=document.getElementById("my-button");
-let anc=document.getElementById("anc");
-let count=0;
-ham.addEventListener("click",()=>{
-    count++;
-    if(count%2==1){
-        anc.style.display="block"
-        // count=0;
-    }
-    else if(count%2==0){
-        anc.style.display="none"
-    }    
+const header = document.querySelector("header");
+
+window.addEventListener ("scroll", function() {
+	header.classList.toggle ("sticky", window.scrollY >0);
+});
+
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+	menu.classList.toggle('bx-x');
+	navbar.classList.toggle('active');
+};
+
+window.onscroll = () => {
+	menu.classList.remove('bx-x');
+	navbar.classList.remove('active');
+};
+
+const sr = ScrollReveal ({
+	distance: '25px',
+	duration: 2000,
+	reset: true
 })
+
+sr.reveal('.home-text',{delay:10, origin:'bottom'})
+
+sr.reveal('.about,.services,.portfolio,.contact',{delay:20, origin:'bottom'})
